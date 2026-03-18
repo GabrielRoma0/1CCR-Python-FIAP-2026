af0dd53496dcfbc087ef32eec2621756182871b1
GabrielRoma0 <gabrielbromao@gmail.com>
2026-03-18 20:47:23 -0300

    aula 2 variáveis

---
+++ Aula02/Desafios.py
@@ -0,0 +1,14 @@
+#1
+nome = input('Digite o seu nome :')
+print(f'Bem-vindo à {nome}')
+
+#2
+dia = int(input('Digite o seu dia de nascimento :'))
+mes = int(input('Digite o seu mes de nascimento :'))
+ano = int(input('Digite o seu ano de nascimento :'))
+print(f'Você nasceu dia {dia}/{mes}/{ano}')
+
+#3
+n1 = float(input('Digite o primeiro número :'))
+n2 = float(input('Digite o segundo número :'))
+print(f'A soma dos números é : {n1+n2}')
---
+++ Aula02/aula02a-variaveis.py
@@ -0,0 +1,34 @@
+
+print("ola mundo")
+
+print(7+4)
+print("7 + 4")
+print("7" + "4") #CONCATENANDO STRINGS
+
+'''
+    Comentários de 
+    múltiplas 
+    linhas
+
+'''
+
+#VARIÁVEIS
+nome = "Gabriel" #str
+idade = 19 #int
+peso = 76.3 #float
+
+print("Oiiii", nome, idade,peso)
+print(f'Oiiii {nome} !!!!')
+
+nome = input("Digite o seu nome :")
+idade = int(input("Digite a sua idade :"))
+peso = float(input("Digite seu peso :"))
+
+print(f"{nome}\n{idade}\n{peso}\n")
+
+
+ano_nascimento = 2006
+ano_atual = 2026
+idade1 = ano_atual - ano_nascimento
+
+print(idade1)
